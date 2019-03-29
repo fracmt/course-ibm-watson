db3bc7888a5ceb081be673b285bb90b2800e47b4
Francisco Torres <fracmt@Franciscos-MacBook-Pro-2.local>
2019-03-28 21:00:15 -0300

    Return http image of watson when dialog anything_else is called

--- watson-telegram-bot/index.js
+++ watson-telegram-bot/index.js
@@ -5,7 +5,7 @@ let context = {};
 
 const watsonAssistant = new AssistantV1({
   "version": "2018-07-10",
-  "iam_apikey": "your_watson_api_key",
+  "iam_apikey": "your_watson_api_key-RHNAQs0AmzcOWlccEeEx",
   "iam_apikey_description": "Auto generated apikey during resource-key operation for Instance - crn:v1:bluemix:public:conversation:us-east:a/60c90d29ecae487b93d92313c7ab535f:4ae7e7fa-ba97-40e3-836e-afce9fa98e58::",
   "iam_apikey_name": "auto-generated-apikey-154c1a01-ae97-4d10-88cd-f8ddc5d23787",
   "iam_role_crn": "crn:v1:bluemix:public:iam::::serviceRole:Manager",
@@ -32,10 +32,17 @@ bot.on('message', (msg) => {
     context,
 };
   watsonAssistant.message(params, (err, response) => {
-    //console.log(JSON.stringify(response, null, "\t"));
     if(err)
           bot.sendMessage(msg.chat.id, 'Eita... deu algum  erro na API :S');
-    bot.sendMessage(msg.chat.id, response.output.text.join('\n'));
+
+    let dialog = response.output.generic[0];
+
+    if(dialog.response_type == 'image') {
+        const photo = dialog.source;
+        bot.sendPhoto(msg.chat.id, photo, {caption: dialog.title});
+    } else {
+        bot.sendMessage(msg.chat.id, response.output.text.join('\n'));
+    }
 
     context = response.context;
 });
